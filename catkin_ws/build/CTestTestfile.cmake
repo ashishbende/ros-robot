373793d8116a5979d36216c5ef30bf902d6aadf3
ashish bende <ashishbende@cnollege.net>
2019-08-25 10:06:47 -0700

    Initial commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ashish/Lab/Robotics/ros-robot/catkin_ws/src
+# Build directory: /home/ashish/Lab/Robotics/ros-robot/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
